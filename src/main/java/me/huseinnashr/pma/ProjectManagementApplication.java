8aa017475069201a34ea66cc4d16032f2f11de65
Husein <nvdhunter007@gmail.com>
2020-06-19 01:24:12 +0700

    rename boot class to ProjectManagementApplication

--- src/main/java/me/huseinnashr/pma/ProjectManagementApplication.java
+++ src/main/java/me/huseinnashr/pma/ProjectManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ProjectManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ProjectManagementApplication.class, args);
 	}
 
 }
