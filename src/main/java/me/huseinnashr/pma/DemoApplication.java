b5620591f8ec3b928a12a35dbbcf18ba050a4503
Husein <nvdhunter007@gmail.com>
2020-06-19 00:51:50 +0700

    init boot project

---
+++ src/main/java/me/huseinnashr/pma/DemoApplication.java
@@ -0,0 +1,13 @@
+package me.huseinnashr.pma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
