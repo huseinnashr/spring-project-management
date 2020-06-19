9bfe119841b9ef381abc7026b9f152a5324935e5
Husein <nvdhunter007@gmail.com>
2020-06-19 15:33:48 +0700

    show all employees

--- src/main/java/me/huseinnashr/pma/controllers/HomeController.java
+++ src/main/java/me/huseinnashr/pma/controllers/HomeController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import me.huseinnashr.pma.dao.EmployeeRepository;
 import me.huseinnashr.pma.dao.ProjectRepository;
+import me.huseinnashr.pma.entities.Employee;
 import me.huseinnashr.pma.entities.Project;
 
 @Controller
@@ -18,11 +20,16 @@ public class HomeController {
   @Autowired
   ProjectRepository proRepo;
 
+  @Autowired
+  EmployeeRepository empRepo;
+
   @GetMapping
   public String displayHome(Model model) {
     List<Project> projects = proRepo.findAll();
-
     model.addAttribute("projects", projects);
+
+    List<Employee> employees = empRepo.findAll();
+    model.addAttribute("employees", employees);
 
     return "home";
   }
--- src/main/java/me/huseinnashr/pma/dao/EmployeeRepository.java
+++ src/main/java/me/huseinnashr/pma/dao/EmployeeRepository.java
@@ -1,8 +1,12 @@
 package me.huseinnashr.pma.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import me.huseinnashr.pma.entities.Employee;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+  @Override
+  public List<Employee> findAll();
 }
