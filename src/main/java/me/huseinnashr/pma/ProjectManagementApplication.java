95b4e13ec5db6d364959c47c98ef325143927b2f
Husein <nvdhunter007@gmail.com>
2020-06-20 17:46:12 +0700

    moving bean out of main to dedicated config class

--- src/main/java/me/huseinnashr/pma/ProjectManagementApplication.java
+++ src/main/java/me/huseinnashr/pma/ProjectManagementApplication.java
@@ -3,14 +3,9 @@ package me.huseinnashr.pma;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import me.huseinnashr.pma.dao.EmployeeRepository;
 import me.huseinnashr.pma.dao.ProjectRepository;
-import me.huseinnashr.pma.springExample.Car;
-import me.huseinnashr.pma.springExample.Doors;
-import me.huseinnashr.pma.springExample.Engine;
-import me.huseinnashr.pma.springExample.Tires;
 
 @SpringBootApplication
 public class ProjectManagementApplication {
@@ -20,14 +15,6 @@ public class ProjectManagementApplication {
 
 	@Autowired
 	ProjectRepository proRepo;
-
-	@Bean
-	public Car newCar() {
-		Engine e = new Engine();
-		Doors d = new Doors();
-		Tires t = new Tires();
-		return new Car(e, d, t);
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagementApplication.class, args);
---
+++ src/main/java/me/huseinnashr/pma/springExample/ManufactureConfig.java
@@ -0,0 +1,15 @@
+package me.huseinnashr.pma.springExample;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ManufactureConfig {
+  @Bean
+  public Car newCar() {
+    Engine e = new Engine();
+    Doors d = new Doors();
+    Tires t = new Tires();
+    return new Car(e, d, t);
+  }
+}
