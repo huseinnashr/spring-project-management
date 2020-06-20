5e4073c18892f13d17505c9f3c8ef88201c01910
Husein <nvdhunter007@gmail.com>
2020-06-21 02:55:24 +0700

    test environment the right way

--- src/test/java/me/huseinnashr/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/me/huseinnashr/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -5,23 +5,13 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlGroup;
-import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import me.huseinnashr.pma.ProjectManagementApplication;
 import me.huseinnashr.pma.entities.Project;
 
-@ContextConfiguration(classes = ProjectManagementApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
-@SqlGroup({
-		@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = { "classpath:db_data.sql",
-				"classpath:db_schema.sql" }),
-		@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:db_drop.sql") })
 public class ProjectRepositoryIntegrationTest {
 	@Autowired
 	ProjectRepository proRepo;
--- src/test/resources/db_drop.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE project_employee IF EXISTS;
-DROP TABLE project IF EXISTS;
-DROP TABLE employee IF EXISTS;
