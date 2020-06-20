9f977a0f5cae80f10afac6c22a9869f3b0eff683
Husein <nvdhunter007@gmail.com>
2020-06-20 15:20:33 +0700

    seed db with the data.sql

--- src/main/java/me/huseinnashr/pma/entities/Employee.java
+++ src/main/java/me/huseinnashr/pma/entities/Employee.java
@@ -15,7 +15,7 @@ import javax.persistence.ManyToMany;
 @Entity
 public class Employee {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long employeeId;
 
   private String firstName;
--- src/main/java/me/huseinnashr/pma/entities/Project.java
+++ src/main/java/me/huseinnashr/pma/entities/Project.java
@@ -16,7 +16,7 @@ import javax.persistence.ManyToMany;
 @Entity
 public class Project {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long projectId;
   private String name;
   private String stage;
