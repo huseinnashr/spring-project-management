66dd896f5b3856a7503b5012ec326778e5d93c46
Husein <nvdhunter007@gmail.com>
2020-06-21 17:28:16 +0700

    reset db every test method

--- src/test/java/me/huseinnashr/pma/controllers/HttpRequestTest.java
+++ src/test/java/me/huseinnashr/pma/controllers/HttpRequestTest.java
@@ -9,10 +9,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SqlGroup({
+    @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = { "classpath:db_schema.sql",
+        "classpath:db_data.sql" }),
+    @Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:db_drop.sql") })
 public class HttpRequestTest {
   @LocalServerPort
   private int port;
--- src/test/java/me/huseinnashr/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/me/huseinnashr/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -6,12 +6,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import me.huseinnashr.pma.entities.Project;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@SqlGroup({
+		@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = { "classpath:db_schema.sql",
+				"classpath:db_data.sql" }),
+		@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:db_drop.sql") })
 public class ProjectRepositoryIntegrationTest {
 	@Autowired
 	ProjectRepository proRepo;
